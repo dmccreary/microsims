12c37ca6c269d8d2a1e928ecad209e9476a4bee9
Dan Mccreary <Dan.McCreary@gmail.com>
2023-11-25 00:18:07 -0600

    Deployed 579569c with MkDocs version: 1.5.3

---
+++ sims/a-star/a-star.js
@@ -0,0 +1,28 @@
+
+// Global variables for canvas dimensions
+let canvasWidth = 800;
+let canvasHeight = 600;
+
+function setup() {
+    // Canvas setup
+    createCanvas(canvasWidth, canvasHeight);
+    textSize(16);
+    background(245);
+
+    // Initialize buttons and sliders
+    initControls();
+}
+
+function draw() {
+    // Main drawing loop
+}
+
+function initControls() {
+    // Function to initialize buttons and sliders
+    // Buttons: Start, Step Forward, Step Back, Reset
+    // Sliders: TBD based on A* algorithm parameters
+}
+
+// A* algorithm implementation goes here
+
+// Additional functions for graph creation, drawing, and interaction go here
