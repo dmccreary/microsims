351cd32e474919a89b2e38efe3297483287ede33
Dan Mccreary <Dan.McCreary@gmail.com>
2024-10-06 19:48:42 -0500

    Deployed 243a380 with MkDocs version: 1.6.0

--- sims/slope-and-intercept/slope.js
+++ sims/slope-and-intercept/slope.js
@@ -1,45 +1,71 @@
+let canvasWidth = 400;
+let drawAreaHeight = 400;
+let canvasHeight = 450;
+let controlHeight = 70;
+
 let slopeSlider;
 let interceptSlider;
 
 function setup() {
-  createCanvas(400, 400);
+  const canvas = createCanvas(canvasWidth, canvasHeight);
+  // canvas.parent('canvas-container');
+  var mainElement = document.querySelector('main');
+  canvas.parent(mainElement);
+  textSize(16);
+
   slopeSlider = createSlider(-1, 1, .5, 0.01);
-  slopeSlider.position(95, 10);
-  slopeSlider.style('width', '310px');
+  slopeSlider.position(115, drawAreaHeight + 10);
+  slopeSlider.size(canvasWidth - 120);
 
   interceptSlider = createSlider(0, 400, 100, 1);
-  interceptSlider.position(95, 30);
-  interceptSlider.style('width', '310px');
+  interceptSlider.position(115, drawAreaHeight + 30);
+  interceptSlider.size(canvasWidth - 120);
+
 }
 
 function draw() {
-  background(250);
+  noStroke();
+  fill("aliceblue");  // DodgerBlue background
+  rect(0, 0, canvasWidth, drawAreaHeight)
+  
+  // Draw the background in a light tan color
+  fill('cornsilk')
+  stroke(1);
+  strokeWeight(1);
+  rect(0, drawAreaHeight, canvasWidth, controlHeight)
+
   stroke('black');
   strokeWeight(1);
   let slopeValue = slopeSlider.value();
   let interceptValue = interceptSlider.value();
-  text('Slope: ' + slopeValue, 5, 15);
-  text('Intercept: ' + interceptValue, 5, 35);
+  fill('black');
+  strokeWeight(1);
+  text('Slope: ' + slopeValue, 5, drawAreaHeight + 18);
+  text('Intercept: ' + interceptValue, 5, drawAreaHeight + 38);
   
-  // Drawing grid
-
-  for(let i = 0; i <= width; i += 50){
-    if(i % 100 === 0){
-      strokeWeight(1);
-      line(i, 0, i, height);
-      line(0, i, width, i);
-    } else {
-      strokeWeight(1);
-      drawingContext.setLineDash([5, 15]);
-      line(i, 0, i, height);
-      line(0, i, width, i);
-      drawingContext.setLineDash([]);
-    }
-  }
+  drawGrid();
   
   // Drawing blue line
   stroke('blue');
   strokeWeight(4);
 
   line(0, 400 - interceptValue, width, 400 - interceptValue - slopeValue * width);
+}
+
+function drawGrid() {
+  // Drawing grid
+  // stroke('gray');
+  for(let i = 0; i <= canvasWidth; i += 50){
+    if(i % 100 === 0){
+      strokeWeight(1);
+      line(i, 0, i, drawAreaHeight);
+      line(0, i, canvasWidth, i);
+    } else {
+      strokeWeight(1);
+      drawingContext.setLineDash([5, 15]);
+      line(i, 0, i, drawAreaHeight);
+      line(0, i, canvasWidth, i);
+      drawingContext.setLineDash([]);
+    }
+  }
 }
