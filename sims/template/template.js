d4351df957021619e76d14a86f859ebab2c2a951
Dan Mccreary <Dan.McCreary@gmail.com>
2024-10-19 22:42:08 -0500

    Deployed 237b3d1 with MkDocs version: 1.6.0

--- sims/template/template.js
+++ sims/template/template.js
@@ -1,4 +1,4 @@
-// region drawing parameters
+// MicroSim Template with region drawing parameters
 // the width of the entire canvas
 let canvasWidth = 400;
 // The top drawing region above the interactive controls
@@ -6,7 +6,7 @@ let drawHeight = 400;
 // control region height
 let controlHeight = 50;
 // The total hight of both the drawing region height + the control region height
-let canvasHeight = 450;
+let canvasHeight = drawHeight + controlHeight;
 // margin around the active plotting region
 let margin = 25;
 // larger text so students in the back of the room can read the labels
@@ -21,7 +21,7 @@ function setup {
 
     // create a new slider at th bottom of the canvas
     mySlider = createSlider(0, 100, 50, 1);
-    mySlider.position(sliderLeftMargin, drawHeight + 12);
+    mySlider.position(sliderLeftMargin, drawHeight + 30);
     mySlider.size(canvasWidth - 2*margin); 
 }
 
@@ -38,7 +38,6 @@ function draw() {
 
     // put your custom code here
     
-
     // draw label and value
     text("MySlider: " +  val, 10, drawHeight + 25)
 }
