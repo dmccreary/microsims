6378e9535950e3cfdb1522f4adba4100314972e1
Dan Mccreary <Dan.McCreary@gmail.com>
2024-10-19 20:52:30 -0500

    updating content

--- docs/sims/template/template.js
+++ docs/sims/template/template.js
@@ -1,35 +1,43 @@
+// region drawing parameters
+// the width of the entire canvas
 let canvasWidth = 400;
+// The top drawing region above the interactive controls
 let drawHeight = 400;
+// control region height
+let controlHeight = 50;
+// The total hight of both the drawing region height + the control region height
 let canvasHeight = 450;
-let sliderLeftMargin = 120;
+// margin around the active plotting region
+let margin = 25;
+// larger text so students in the back of the room can read the labels
+let defaultTextSize = 16;
 
 function setup {
     const canvas = createCanvas(canvasWidth, canvasHeight);
     // canvas.parent('canvas-container');
     var mainElement = document.querySelector('main');
     canvas.parent(mainElement);
-    textSize(16);
+    textSize(defaultTextSize);
 
     // create a new slider at th bottom of the canvas
     mySlider = createSlider(0, 100, 50, 1);
     mySlider.position(sliderLeftMargin, drawHeight + 12);
-    mySlider.style('width', canvasWidth -  sliderLeftMargin - 20 + 'px');
- 
+    mySlider.size(canvasWidth - 2*margin); 
 }
 
 function draw() {
-    // make the background drawing region light gray
+    // make the background drawing region a very light blue
     fill('aliceblue');
-    rect(0, 0, canvasWidth, canvasWidth);
-    // make the background of the controls white
+    rect(0, 0, canvasWidth, drawHeight);
+    // make the background of the controls area white
     fill('white')
-    rect(0, drawHeight, canvasWidth, canvasHeight-drawHeight);
+    rect(0, drawHeight, canvasWidth, controlHeight);
 
     // get the updated slider value
     val = mySlider.value();
 
-    // draw the sim here
-    circle(canvasWidth/2, drawHeight/2, val)
+    // put your custom code here
+    
 
     // draw label and value
     text("MySlider: " +  val, 10, drawHeight + 25)
