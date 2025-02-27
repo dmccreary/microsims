e32e21312770aaca022e5c6941e4e6aaf057f8dd
Dan Mccreary <Dan.McCreary@gmail.com>
2025-02-27 06:36:18 -0600

    updating content

--- docs/sims/resize-in-iframe/resize-width.js
+++ docs/sims/resize-in-iframe/resize-width.js
@@ -1,15 +1,17 @@
 // Responsive width - resize to the width of the container
 
 // global variables
-let container;
+let container; // The HTML element that contains the canvas
+let canvasWidth = 400; // Elastic width of container
 let canvasHeight = 400; // Maintain the fixed height
 
 function setup() {
-  // Select the container dynamically
+  // Select the container dynamically using the document object
+  // make sure you have a <main> element in your HTML
   container = document.querySelector('main');
   
   // Create a canvas with the same width as the container and a fixed height
-  const canvasWidth = container.offsetWidth;
+  canvasWidth = container.offsetWidth;
   let canvas = createCanvas(canvasWidth, canvasHeight);
   
   // Attach the canvas to the container
@@ -26,11 +28,11 @@ function draw() {
   
   // Display the canvas width and height as text in center of canvas
   const canvasWidth = container.offsetWidth;
-  text(`Width: ${canvasWidth}px\nHeight: ${height}px`, canvasWidth / 2, canvasHeight / 2);
+  text(`Width: ${canvasWidth}px\nHeight: ${canvasHeight}px`, canvasWidth / 2, canvasHeight / 2);
 }
 
 function windowResized() {
   // Update the canvas width when the window is resized
-  const canvasWidth = container.offsetWidth;
+  canvasWidth = container.offsetWidth;
   resizeCanvas(canvasWidth, canvasHeight);
 }
--- docs/sims/template/index.md
+++ docs/sims/template/index.md
@@ -1,12 +1,12 @@
 # MicroSim Template
 
-<iframe src="template.html" height="200px"></iframe>
+<iframe src="main.html" height="200px"></iframe>
 
 <!--
 ![Image Name](./image.png){ width="400" }
 -->
 
-[Run the MicroSim Template](./template.html){ .md-button .md-button--primary }
+[Run the MicroSim](./html.html){ .md-button .md-button--primary }
 [Edit this MicroSim](https://editor.p5js.org/dmccreary/sketches/dJq4nTXE4)
 
 ## Sample iframe
